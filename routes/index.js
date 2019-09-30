4defef57e481b51ce73adb88f35e498b388bbff8
David <davidhaw2005@gmail.com>
2019-09-29 21:47:01 -0500

    Finished Navbar Navbar is finished. it doesn't look the best, but that can be customized to the point where the whole website looks good later.

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', {title: 'Linguist Master'});
 });
+router.get('/index', function (req, res, next) {
+  res.render('index', {title: 'Linguist Master'});
+});
 
+/* GET language pages */
 router.get('/spanish', function (req, res, next) {
   res.render('spanish', {title: 'Spanish'});
 });
